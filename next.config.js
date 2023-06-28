fd78256c403e0cbfe451dd88046f0366c48f3387
Muhammad Asadullah <masdullah230@gmail.com>
2023-06-29 02:35:29 +0500

    redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/asadullah',
+          permanent: true, // 301 Permanent Redirect
+        },
+        {
+            source: '/asadullah/cv',
+            destination: '/AsadullahCV.pdf',
+            permanent: true, // 301 Permanent Redirect
+          },
+      ];
+    },
+  };
+  
